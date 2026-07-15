6661d3f92263da61465d60b473f16466a8828194
unknown <1845712931@qq.com>
2026-07-15 15:45:08 +0800

    按键控制led

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -203,6 +203,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
+  /*Configure GPIO pin : PA1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
   /* USER CODE END MX_GPIO_Init_2 */
--- User/app.c
+++ User/app.c
@@ -6,18 +6,30 @@
 extern UART_HandleTypeDef huart1;
 
 void app_main(void){
-	uint32_t count = 0;
+	uint32_t key_count = 0;
 	char message[64];
+	GPIO_PinState last_key_state = GPIO_PIN_SET;
 
-	const char *start_message = "STM32 USART1 test start\r\n";
+	const char *start_message = "PA1 key test start\r\n";
 	HAL_UART_Transmit(&huart1, (uint8_t *)start_message, strlen(start_message), HAL_MAX_DELAY);
 
 	while (1)
 	{
-		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
-		count++;
-		snprintf(message, sizeof(message), "LED Toggle: %lu\r\n", count);
-		HAL_UART_Transmit(&huart1, (uint8_t *)message, strlen(message), HAL_MAX_DELAY);
-		HAL_Delay(500);
+		GPIO_PinState key_state = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_1);
+
+		if (last_key_state == GPIO_PIN_SET && key_state == GPIO_PIN_RESET)
+		{
+			HAL_Delay(20);
+			if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_1) == GPIO_PIN_RESET)
+			{
+				HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+				key_count++;
+				snprintf(message, sizeof(message), "Key Pressed: %lu\r\n", key_count);
+				HAL_UART_Transmit(&huart1, (uint8_t *)message, strlen(message), HAL_MAX_DELAY);
+			}
+		}
+
+		last_key_state = key_state;
+		HAL_Delay(10);
 	}
 }
